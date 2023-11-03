5d96de243554c25e61c6e94fe30d5a19b71cfe94
FeliciaChiu <132633991+FeliciaChiu@users.noreply.github.com>
2023-11-03 09:14:15 -0700

    second edit check!

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Just kidding, bye.");     
         System.out.println("Hiiiiii!");
+        System.out.println("hi :D");
     }
 }
