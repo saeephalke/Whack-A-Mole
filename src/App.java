d1eed0509fdd19b80f0da5bf180e32ac57946af1
saeephalke <91033007+saeephalke@users.noreply.github.com>
2023-11-07 09:22:22 -0800

    testing whackString

--- src/App.java
+++ src/App.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! Just kidding, bye.");     
-        System.out.println("Hiiiiii!");
-        System.out.println("hi :D");
+        WhackString s = new WhackString();
+        System.out.println(s.createString());
     }
 }
