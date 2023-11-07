24c4df0a7f003b2b15e391adaadb7355a2f67be0
happysisyphus2 <149814218+happysisyphus2@users.noreply.github.com>
2023-11-07 09:12:13 -0800

    Update WhackString.java

--- src/WhackString.java
+++ src/WhackString.java
@@ -1,1 +1,19 @@
+public WhackString {
+//Prints out the String with the mole in it
+    private String whackString = "";
+    private int index;
+    index = (int)(Math.random() * 5 + 1);
 
+    public String createString () {
+      for (i = 0; i < 5; i++) {
+          if (i != index) {
+              String += "-";
+          }
+          else {
+              String += "$";
+          }
+      }
+      return String;
+
+  }
+}
